d8ac0981d93510f33fd8152eb0694400984d7ccd
Joaszek <joachim.szewior@gmail.com>
2023-02-16 20:36:33 +0100

    init

---
+++ src/main/java/com/example/blockchainsimulation/BlockchainSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.example.blockchainsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blockchainsimulation/BlockchainSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blockchainsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
